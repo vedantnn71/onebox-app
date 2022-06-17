56d483908b9492b8630b3f83ec0e9e24769068d6
VedantNandwana <>
2022-06-17 04:40:46 +0000

    Implement: facebook oauth

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import FacebookProvider from "next-auth/providers/facebook";
 import clientPromise from "./lib/mongodb";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
@@ -11,6 +12,10 @@ export default NextAuth({
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
 });
