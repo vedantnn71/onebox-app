9ac61bf8b1c5297dc60c9d27e29ee2f86dcc8943
Vedant Nandwana <70624701+vedantnn71@users.noreply.github.com>
2022-06-18 09:06:05 +0000

    Update: implement connect oauth modal

--- src/components/connectOAuthModal.tsx
+++ src/components/connectOAuthModal.tsx
@@ -5,33 +5,71 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton,
-  useDisclosure
+  Button,
+  Flex,
 } from "@chakra-ui/react";
 import { FC } from "react";
 import { signIn } from "next-auth/react";
+import Icon from "./icon";
 
 interface ConnectOAuthModalProps {
-  providerId: "twitter" | "facebook" | "google";
+  providerId: string;
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
 }
 
-const ConnectOAuthModal: FC<ConnectOAuthModalProps> = ({ providerId, isOpen, onClose }) => { 
+const ConnectOAuthModal: FC<ConnectOAuthModalProps> = ({
+  providerId,
+  isOpen,
+  onClose,
+}) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Connect your account</ModalHeader>
-        <ModalBody>To access your {providerId} through onebox, please link your {providerId} account.</ModalBody>
+      <ModalContent
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+      >
+        <ModalHeader
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Icon name={`bxl-${providerId}`} color="blue.600" size="32px" />
+          Connect your account
+        </ModalHeader>
+        <ModalBody>
+          To access your {providerId} through onebox, please link your{" "}
+          {providerId} account.
+        </ModalBody>
+        <Flex direction="row" gap="4" paddingX="6" paddingY="4">
+          <Button
+            variant="outline"
+            colorScheme="gray"
+            border="2px"
+            onClick={onClose}
+            size="sm"
+            paddingX="6"
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={() => signIn(providerId)}
+            colorScheme="blue"
+            paddingX="6"
+            size="sm"
+          >
+            Connect
+          </Button>
+        </Flex>
       </ModalContent>
-      <ModalFooter>
-        <ModalCloseButton>Cancel</ModalCloseButton>         
-      </ModalFooter>  
     </Modal>
-      
-  )
-}
+  );
+};
 
 export default ConnectOAuthModal;
