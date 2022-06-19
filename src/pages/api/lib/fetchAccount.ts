add55404ac7d4c3ebddc8bcc14272a082e68d5ae
Vedant Nandwana <70624701+vedantnn71@users.noreply.github.com>
2022-06-19 09:37:06 +0000

    Update: implement `api/lib/fetchAccounts`

---
+++ src/pages/api/lib/fetchAccount.ts
@@ -0,0 +1,34 @@
+import type { Db, Collection, WithId } from "mongodb";
+import { ObjectId } from "mongodb";
+import mongoClient from "./mongoClient";
+
+interface Account {
+  provider: string;
+  type: string;
+  providerAccountId?: string;
+  access_token?: string;
+  token_type?: string;
+  userId: ObjectId;
+  expires_at: number;
+  oauth_token?: string;
+  oauth_token_secret?: string;
+}
+
+
+const fetchAccount = (userId: string): Promise<WithId<Account>> | null => {
+  if (!userId || !(ObjectId.isValid(userId))) {
+    throw new Error("Invalid userId")
+  }
+
+  const database: Db = mongoClient.db("main");
+  const accounts: Collection<Account> = database.collection("main");
+  const account = accounts.findOne({ userId: new ObjectId(userId) });
+
+  if (!account) {
+    return null;
+  }
+
+  return account;
+}
+
+export default fetchAccount;
