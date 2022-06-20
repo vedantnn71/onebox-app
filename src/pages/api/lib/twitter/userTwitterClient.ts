8cb2e0c2bbe9dd6f3302f3154f9e21f9aa4df74a
VedantNandwana <>
2022-06-20 17:27:46 +0000

    Update: implement

---
+++ src/pages/api/lib/twitter/userTwitterClient.ts
@@ -0,0 +1,28 @@
+import type { Db, Collection } from "mongodb";
+import { ObjectId } from "mongodb";
+import { TwitterClient } from "twitter-api-client";
+import mongoClient from "../mongoClient";
+import Account from "../interfaces/account";
+
+const userTwitterClient = async (accountId: ObjectId) => {
+  const client = await mongoClient;
+  const database: Db = await client.db("main");
+  const accounts: Collection<Account> = await database.collection("accounts");
+  const account = await accounts.findOne({ _id: accountId });
+
+  if (!account) {
+    throw new Error("Account not found");
+  }
+
+  const { oauth_token, oauth_token_secret } = account;
+  const twitterClient = new TwitterClient({
+    apiKey: process.env.TWITTER_CLIENT_ID, 
+    apiSecret: process.env.TWITTER_CLIENT_SECRET, 
+    accessToken: oauth_token,
+    accessTokenSecret: oauth_token_secret
+  })
+
+  return twitterClient;
+}
+
+export default userTwitterClient;
