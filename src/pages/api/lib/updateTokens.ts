fa34fdb5f5940a8a7148080cb45cc74e70a9c24a
VedantNandwana <>
2022-06-20 06:38:06 +0000

    Update: implement

---
+++ src/pages/api/lib/updateTokens.ts
@@ -0,0 +1,27 @@
+import type { Db, Collection } from "mongodb";
+import { ObjectId } from "mongodb";
+import mongoClient from "./mongoClient";
+import Account from "./interfaces/account";
+
+interface IUpdateTokens {
+  accountId: ObjectId;
+  accessToken: string;
+  refreshToken: string;
+}
+
+const updateTokens = async ({
+  accountId,
+  accessToken,
+  refreshToken
+}: IUpdateTokens) => {
+  const client = await mongoClient;
+  const database: Db = await client.db("main");
+  const accounts: Collection<Account> = await database.collection("accounts");
+  const account = await accounts.findOneAndUpdate({ _id: accountId }, {
+    $set: { access_token: accessToken, refresh_token: refreshToken },
+  });
+
+  return account;
+};
+
+export default updateTokens;
