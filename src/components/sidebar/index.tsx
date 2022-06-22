dbc6e45176ccfab432e5cde705c46dd576561c28
VedantNandwana <>
2022-06-22 05:40:15 +0000

    Update: enable prop drilling for toggling inbox, implement basic inbox ui

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,13 +1,18 @@
 import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
 import Navigation from "./navigation";
 import Inbox from "./inbox";
 
 const Sidebar = () => {
+  const [inbox, setInbox] = useState<{
+    show: boolean,
+    provider: "twitter" | "facebook"
+  }>({ show: false, provider: "twitter" })
 
   return (
-    <Flex direction="row">
-      <Navigation />
-      <Inbox />
+    <Flex direction="row" maxH="100vh">
+      <Navigation inbox={inbox} setInbox={setInbox} />
+      {inbox.show && <Inbox provider={inbox.provider} />}
     </Flex>
   );
 };
